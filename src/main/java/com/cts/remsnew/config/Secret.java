ae90d55896508b07b1635fcbf2dc93979de4a3a2
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-17 11:01:50 +0530

    Create Secret.java

---
+++ src/main/java/com/cts/remsnew/config/Secret.java
@@ -0,0 +1,9 @@
+package com.remsnew.config;
+
+public class Secret {
+
+	public static String SECRET_KEY = "T1[oK]cY*9|X3W!z$H@D#5&F)J@NcRfUjXn2r5u8x/A?D(G+KbPeShVmYq3t6w9";
+	public static long EXPIRATION_TIME = 1800000; // 1 hour
+	
+}
+ 
