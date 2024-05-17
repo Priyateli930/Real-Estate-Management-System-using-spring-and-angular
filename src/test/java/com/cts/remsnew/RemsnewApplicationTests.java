b2ae901eaa6ba09dda61782e8b02b679ef30aa6a
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-17 17:26:42 +0530

    Create RemsnewApplicationTests.java

---
+++ src/test/java/com/cts/remsnew/RemsnewApplicationTests.java
@@ -0,0 +1,14 @@
+package com.remsnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemsnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
