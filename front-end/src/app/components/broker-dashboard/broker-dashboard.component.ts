ec7173d0d9ca81c24897460eeaa0ce678bd12f20
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-17 20:26:59 +0530

    Create broker-dashboard.component.ts

---
+++ front-end/src/app/components/broker-dashboard/broker-dashboard.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Users } from '../../models/users';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-broker-dashboard',
+  templateUrl: './broker-dashboard.component.html',
+  styleUrl: './broker-dashboard.component.css'
+})
+export class BrokerDashboardComponent implements OnInit{
+
+  userName: string='';
+  constructor(private router: Router, private http: HttpClient, private loginService: LoginService) { }
+
+  ngOnInit() {
+    const email = localStorage.getItem('email'); // Get the email from local storage
+
+    if (email) {
+      this.loginService.getUserByEmail(email).subscribe(user => {
+        this.userName = user.username;
+      localStorage.setItem('userId', user.userid);
+      localStorage.setItem('userName', user.username);
+      localStorage.setItem('userType', user.usertype);
+      });
+    }
+  }
+
+  currentView: string = '';
+
+  openNav() {
+    document.getElementById("mySidenav")!.style.width = "300px";
+    (document.querySelector(".main-content") as HTMLElement)!.style.marginLeft = "300px";
+  }
+
+  closeNav() {
+    document.getElementById("mySidenav")!.style.width = "0";
+    (document.querySelector(".main-content") as HTMLElement)!.style.marginLeft = "0";
+  }
+
+
+  // logout() {
+    // your logout logic here
+
+    // then clear localStorage
+  //   localStorage.clear();
+  // }
+}
+
