1d6648bc1241d9c42c335832d3bcaad62cab2bc3
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-17 11:15:52 +0530

    Create TransactionNotFoundException.java

---
+++ src/main/java/com/cts/remsnew/exception/TransactionNotFoundException.java
@@ -0,0 +1,11 @@
+package com.remsnew.exception;
+
+@SuppressWarnings("serial")
+public class TransactionNotFoundException extends Exception{
+
+	public TransactionNotFoundException(String msg)
+	{
+		super(msg);
+	}
+}
+ 
