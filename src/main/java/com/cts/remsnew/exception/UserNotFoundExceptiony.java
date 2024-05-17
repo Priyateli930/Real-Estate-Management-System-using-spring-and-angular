6bf76d89d56594743989ecb0572e3e01db40be93
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-17 11:16:31 +0530

    Create UserNotFoundExceptiony.java

---
+++ src/main/java/com/cts/remsnew/exception/UserNotFoundExceptiony.java
@@ -0,0 +1,11 @@
+package com.remsnew.exception;
+
+@SuppressWarnings("serial")
+public class UserNotFoundException extends Exception{
+
+	public UserNotFoundException(String msg)
+	{
+		super(msg);
+	}
+}
+ 
