bff90af1099edcd15138ecfeacaad03a3e9fe1b0
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-18 11:51:31 +0530

    Create users.ts

---
+++ front-end/src/app/models/users.ts
@@ -0,0 +1,15 @@
+export interface Users {
+  userid?: any;
+  username?: any;
+  usertype?: any;
+  contactno?: any;
+  email?: any;
+  addressline1?: any;
+  addressline2?: any;
+  city?: any;
+  state?: any;
+  pincode?: any;
+  dateofbirth?: any;
+  verified?: any;
+  passwordhash?: any;
+}
