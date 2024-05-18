4918ad8c7700a626f365ef0877807a71a33f5b0b
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-18 11:32:44 +0530

    Create property-transactions.ts

---
+++ front-end/src/app/models/property-transactions.ts
@@ -0,0 +1,9 @@
+export interface PropertyTransactions {
+    transactionid: number;
+    property: any; // Replace 'any' with the type of 'property'
+    buyertenant: any; // Replace 'any' with the type of 'buyertenant'
+    salerentprice: number;
+    transactiondate: string;
+    sellerid: number;
+  }
+  
