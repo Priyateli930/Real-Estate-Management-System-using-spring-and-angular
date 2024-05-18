04dfadb069a4ab28746702e695f6324fb5bc3a71
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-18 11:30:48 +0530

    Create properties.ts

---
+++ front-end/src/app/models/properties.ts
@@ -0,0 +1,18 @@
+export interface Properties {
+    propertyid: any;
+    owner: any; // Replace 'any' with the type of 'owner'
+    agent: any; // Replace 'any' with the type of 'agent'
+    addressline1: any;
+    addressline2: any;
+    state: any;
+    city: any;
+    pincode: any;
+    size: any;
+    price: any;
+    propertytype: any;
+    propstatus: any;
+    images: any;
+    ratings: any[]; // Replace 'any' with the type of 'ratings'
+    transactions: any[]; // Replace 'any' with the type of 'transactions'
+    userHasTransaction:any;
+  }
