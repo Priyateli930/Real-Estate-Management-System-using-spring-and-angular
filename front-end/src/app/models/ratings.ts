0f7f2042f13e0995801d78d66c900bea3b0c1514
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-18 11:34:11 +0530

    Create ratings.ts

---
+++ front-end/src/app/models/ratings.ts
@@ -0,0 +1,8 @@
+export interface Ratings {
+    // ratingid: number;
+    property: any; // Replace 'any' with the type of 'property'
+    user: any; // Replace 'any' with the type of 'user'
+    ratingvalue: number;
+    feedback: string;
+    dateoffeedback: string;
+  }
