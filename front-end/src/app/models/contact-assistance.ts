0078a74030c51d9350426966d9216991212794e4
Priya Suhas Teli <93397034+Priyateli930@users.noreply.github.com>
2024-05-18 11:29:31 +0530

    Create contact-assistance.ts

---
+++ front-end/src/app/models/contact-assistance.ts
@@ -0,0 +1,10 @@
+export interface ContactAssistance {
+    id: number;
+    user: any; // Replace 'any' with the type of 'user'
+    cname: string;
+    cemail: string;
+    phone: string;
+    subject: string;
+    message: string;
+  }
+  
